a4b029fc5c51ed83875590c9b74227bac679c5c2
MehreenZahra <codewithmehreen1@gmail.com>
2024-06-23 17:45:09 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './components/Login'
+import React from 'react';
+import Login from './components/Login/Login';  
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <Login/>
-      </div>
-       
-    </>
-  )
-}
+    <div>
+      <Login/>
+    </div>
+)}
 
-export default App
+export default App;
